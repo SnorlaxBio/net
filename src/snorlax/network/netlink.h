e56218dbfc4dddde92de87e176c3f0abe28cab2b
snorlax <ceo@snorlax.bio>
2024-07-22 17:46:14 +0900

    Update!

--- src/snorlax/network/netlink.h
+++ src/snorlax/network/netlink.h
@@ -82,7 +82,7 @@ extern network_netlink_t * network_netlink_get(void);
 #define network_netlink_req(descriptor, message)                    ((descriptor)->func->req(descriptor, message))
 // #define network_netlink_wait(descriptor, message)                   ((descriptor)->func->wait(descriptor, message))
 
-#define network_netlink_message_ok(message, len)                    NLMSG_OK(message, ln)
+#define network_netlink_message_ok(message, len)                    NLMSG_OK(message, len)
 #define network_netlink_message_next(message, len)                  NLMSG_NEXT(message, len)
 
 #define netlink_protocol_data_get(type, address, len)               ((type)(((void *) address) + NLMSG_ALIGN(len)))
